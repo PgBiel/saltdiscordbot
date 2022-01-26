e9f17b9872e906ebd766746b122e7e6c0914dc3a
Pg Biel <pgtransformiceandhacks@gmail.com>
2022-01-25 22:40:21 -0300

    Add logging

--- src/main/java/io/github/PgBiel/saltdiscordbot/SaltBot.java
+++ src/main/java/io/github/PgBiel/saltdiscordbot/SaltBot.java
@@ -6,13 +6,18 @@ import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
 import io.github.PgBiel.saltdiscordbot.config.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
 
 public final class SaltBot {
+    public static final Logger LOGGER = LoggerFactory.getLogger(SaltBot.class);
+
     public static void main(final String[] args) {
 //        final String token = args[0];
+        LOGGER.info("Salt is booting up!");
         Config config = Config.getInstance();
         final DiscordClient client = DiscordClient.create(config.getToken());
         final GatewayDiscordClient gateway = client.login().block();
@@ -25,7 +30,10 @@ public final class SaltBot {
                     if (channel != null) channel.createMessage("Pong!").block();
                 }
             });
+            LOGGER.info("Salt's up!");
             gateway.onDisconnect().block();
+        } else {
+            LOGGER.error("Could not get gateway... something is very wrong.");
         }
 
     }
